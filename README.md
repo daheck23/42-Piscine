71dbc06ee4c5ac206a77066063fe982c0836577d
daheck23 <dheck23@icloud.com>
2025-06-01 14:39:18 +0200

    duck
    duck.c

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 42-Piscine
 My 42 piscine exercices
+
+duck.c a little gimmick for the duck treasure hunt 
---
+++ duck.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+	int i = 0;
+	while(i < 5)
+	{
+		printf("        .--''--.\n");
+		printf("       /        \\\n");
+		printf("      |  O     O |\n");
+		printf("      \\   .--.   /\n");
+		printf("       '. \\__/ .'\n");
+		printf("         `------'\n");
+		printf("      _.--\"\"--._\n");
+		printf("     .'          '.\n");
+		printf("    /   .--.  .--.   \\\n");
+		printf("   |    >--<  >--<    |\n");
+		printf("   \\   '--'  '--'   /\n");
+		printf("    `. _______ .'\n");
+		printf("      //_____\\\\     \n");
+		printf("     (( ____ ))    \n");
+		printf("      `------'    \n");
+		printf("\n");
+		printf("Quak Quak 42 Quak Quak\n");
+		i++;
+	}
+    return 0;
+}
